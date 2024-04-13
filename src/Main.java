e6f2456543935f08b0d9885581bc9bf86dd6c3ce
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-04-13 22:30:55 +0530

    checking some basic functionalities of get name

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,15 @@
 public class Main {
-    public static void main(String[] args) {
-        Thread thread = new Thread(() -> {
-
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("We are in thread: " + Thread.currentThread().getName());
+            }
         });
+        System.out.println("We are in thread: " + Thread.currentThread().getName() + " before starting a new thread");
+        thread.start();
+        System.out.println("We are in thread: " + Thread.currentThread().getName() + " after starting a new thread");
+
+        Thread.sleep(1000);
     }
 }
