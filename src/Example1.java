2d99e2a6a625a5d3de1f2f5c4700b84bff43d24c
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-04-13 23:15:33 +0530

    misbehaving threads containing message intentional exception

---
+++ src/Example1.java
@@ -0,0 +1,21 @@
+public class Example1 {
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                throw new RuntimeException("Intentional Exception");
+            }
+        });
+
+        thread.setName("Misbehaving thread");
+
+        thread.setUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
+            @Override
+            public void uncaughtException(Thread t, Throwable e) {
+                System.out.println("A critical happened in thread " + t.getName() + " the error is " + e.getMessage());
+            }
+        });
+
+        thread.start();
+    }
+}
