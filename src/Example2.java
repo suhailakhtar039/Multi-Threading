2f2c348af040c5bcd1c0d77f04400f55f67766d8
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-04-13 23:25:13 +0530

    creating thread using class

---
+++ src/Example2.java
@@ -0,0 +1,14 @@
+public class Example2 {
+    public static void main(String[] args) {
+        Thread thread = new NewThread();
+
+        thread.start();
+    }
+
+    private static class NewThread extends Thread{
+        @Override
+        public void run(){
+            System.out.println("Hello from: " + Thread.currentThread().getName());
+        }
+    }
+}
