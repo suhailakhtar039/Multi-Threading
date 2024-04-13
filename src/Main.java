525c6ab80d53a6d6080826d0d88b2aaf845a54ea
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-04-13 22:55:14 +0530

    setting priority

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,12 @@ public class Main {
             @Override
             public void run() {
                 System.out.println("We are in thread: " + Thread.currentThread().getName());
+                System.out.println("Current thread priority: " + Thread.currentThread().getPriority());
             }
         });
 
         thread.setName("New worker thread");
+        thread.setPriority(Thread.MAX_PRIORITY);
 
         System.out.println("We are in thread: " + Thread.currentThread().getName() + " before starting a new thread");
         thread.start();
