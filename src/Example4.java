ad6f6e1faccf96715d416de62b77fee84a53ebbb
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-04-14 16:07:07 +0530

    Exiting the running thread

---
+++ src/Example4.java
@@ -0,0 +1,18 @@
+public class Example4 {
+    public static void main(String[] args) {
+        Thread thread = new Thread(new BlockingTask());
+        thread.start();
+        thread.interrupt();
+    }
+
+    private static class BlockingTask implements Runnable {
+        @Override
+        public void run() {
+            try {
+                Thread.sleep(50000);
+            } catch (InterruptedException e) {
+                System.out.println("Exiting blocking thread");
+            }
+        }
+    }
+}
