5d4a14cd4aaec5b5ca837d9d572218f32488aef5
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-04-14 16:32:41 +0530

    learned about thread interrupt.

--- src/Example4.java
+++ src/Example4.java
@@ -1,6 +1,12 @@
+import java.math.BigInteger;
+
 public class Example4 {
     public static void main(String[] args) {
-        Thread thread = new Thread(new BlockingTask());
+        // Thread thread = new Thread(new BlockingTask());
+        // thread.start();
+        // thread.interrupt();
+        Thread thread = new Thread(new LongComputationTask(new BigInteger("2"),new BigInteger("5")));
+
         thread.start();
         thread.interrupt();
     }
@@ -15,4 +21,31 @@ public class Example4 {
             }
         }
     }
+
+    private static class LongComputationTask implements Runnable{
+        private BigInteger base;
+        private BigInteger power;
+
+        public LongComputationTask(BigInteger base, BigInteger power) {
+            this.base = base;
+            this.power = power;
+        }
+
+        @Override
+        public void run() {
+            System.out.println(base + "^^" + power +" = " + pow(base, power));
+        }
+        private BigInteger pow(BigInteger base, BigInteger power) {
+            BigInteger result = BigInteger.ONE;
+            for (BigInteger i = BigInteger.ZERO; i.compareTo(power) != 0; i = i.add(BigInteger.ONE)){
+                if(Thread.currentThread().isInterrupted()){
+                    System.out.println("Prematurely interrupted communication");
+                    return BigInteger.ZERO;
+                }
+                result = result.multiply(base);
+            }
+            return result;
+        }
+    }
+
 }
