66c030e7f6d863137d766c87b51adde125729d41
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-04-14 16:01:35 +0530

    Added multiple threads to execute all

--- src/PlayAround.java
+++ src/PlayAround.java
@@ -1,2 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class PlayAround {
+    public static void main(String[] args) {
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("I'm going for a walk");
+            }
+        });
+
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("I'm going to swim");
+            }
+        });
+        List<Runnable> list = new ArrayList<>();
+        list.add(thread1);
+        list.add(thread2);
+
+        for(Runnable runnable: list)
+            runnable.run();
+
+
+        thread1.start();
+        thread2.start();
+        System.out.println("I'm going home");
+    }
+
 }
